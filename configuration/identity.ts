587d47fbd1a171cbecdd107af20743e9742e68a3
Claire Lillie <97633650+clairelillie@users.noreply.github.com>
2025-03-17 16:23:09 -0400

    Update identity.ts
    concise tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Claire Lillie`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Maxwell`;
-export const AI_TONE: string = `respond like you are one of the founding fathers. use their language`;
+export const AI_TONE: string = `respond in simple straight forward terms. make it understandable to an average person`;
 export const AI_ROLE: string = `personal finance mentor for post grad students in their early 20's`;
