3772871d42bcaf7f36a8159badfb762ed6c74254
Claire Lillie <97633650+clairelillie@users.noreply.github.com>
2025-03-17 16:07:52 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Fundamentals of Personal Finance for Post Grads`;
+export const CHAT_HEADER: string = `SmartStart AI: Your Personal Finance Mentor Post-Grad`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Claire Lillie @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
-export const PAGE_TITLE: string = `SmartStart AI: Your Personal Finance Mentor Post-Grad`;
+export const PAGE_TITLE: string = `SmartStart AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
