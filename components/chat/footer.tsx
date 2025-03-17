8f59240cb94202e84a8eed9f310839ac2c4ceb3c
Claire Lillie <97633650+clairelillie@users.noreply.github.com>
2025-03-17 19:47:55 -0400

    Update footer.tsx
    cute footer

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,28 +3,25 @@ import Link from "next/link";
 
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">
-        {/* Left Pane */}
-        <Link href="/terms" className="hover:underline">
-          Terms of Service
-        </Link>
-      </div>
-      <div className="flex-grow text-center">
-        {/* Center Pane */}
-        {FOOTER_MESSAGE}
-      </div>
-      <div className="flex-grow text-right">
-        {/* Right Pane */}
-        {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
-        <a
-          href="http://www.ringel.ai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          powered by ringel.AI
-        </a>
-      </div>
+    <div className="w-full text-xs text-gray-500 border-t border-gray-200 py-3 px-6 flex justify-between items-center">
+      {/* Left - Terms of Service */}
+      <Link href="/terms" className="hover:underline">
+        Terms of Service
+      </Link>
+
+      {/* Center - Footer Message */}
+      <div className="text-center">{FOOTER_MESSAGE}</div>
+
+      {/* Right - Powered by Ringel.AI */}
+      <a
+        href="http://www.ringel.ai"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-600 hover:text-gray-800 transition-colors"
+      >
+        powered by ringel.AI
+      </a>
     </div>
   );
 }
+
